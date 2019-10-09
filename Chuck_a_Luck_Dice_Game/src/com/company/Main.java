da88b36c8b1f984218daac779c9823324856ba51
Conall Tuohy <49997709+Conall-Tuohy@users.noreply.github.com>
2019-10-09 23:19:52 +0100

    Main, Dice and Wallet added for game.

---
+++ Chuck_a_Luck_Dice_Game/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner input = new Scanner( System.in );
+        System.out.println("Please type the type of bet you want to make (Triple, Field, High, Low)");
+        String userInput = input.next();
+        if(userInput == "Triple" || "Field" || "High"|| "Low")
+        {String userInput = input.next();
+            if ()
+                ;
+            else
+                System.out.println("That doesn't seem to be a number that is greater than or equal to two.");
+        } else 	System.out.println("That doesn't seem to be a number.");
+
+    }
+    public static void ResolveBet(String betType){
+
+    }
+}
